ee2b301efe5b09e771038f1974973d9645c19e75
YuvaraajE <59257764+YuvaraajE@users.noreply.github.com>
2022-11-17 21:25:14 +0530

    Fixed Issues

--- Project_Development_Phase/Sprint 3/app.py
+++ Project_Development_Phase/Sprint 3/app.py
@@ -1,10 +1,9 @@
 from flask import render_template,Flask,request
 import numpy as np
 import pickle 
-from sklearn.preprocessing import scale
 app= Flask(__name__, template_folder='templates')
 
-model = pickle.load(open("Rfmodel.pkl",'rb'))
+model = pickle.load(open("model.pkl",'rb'))
 
 
 
